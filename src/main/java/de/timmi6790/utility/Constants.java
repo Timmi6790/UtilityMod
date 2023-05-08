129b38dabe80e81ace4ebf2086d79b8b56dad011
Tim <13711365+Timmi6790@users.noreply.github.com>
2023-05-08 16:09:37 +0200

    Initial commit

---
+++ src/main/java/de/timmi6790/utility/Constants.java
@@ -0,0 +1,7 @@
+package de.timmi6790.utility;
+
+public class Constants
+{
+	public static final String MODID = "utilitymod";
+	public static final String VERSION = "0.1.0";
+}
---
+++ src/main/java/de/timmi6790/utility/UtilityMod.java
@@ -0,0 +1,12 @@
+package de.timmi6790.utility;
+
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Constants.MODID, version = Constants.VERSION)
+public class UtilityMod {
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+}
