e574fc04063c6a4ad842584fe61d043fd2f5b16e
Tim <13711365+Timmi6790@users.noreply.github.com>
2023-05-10 14:23:33 +0200

    Add private constructor to Constants

--- src/main/java/de/timmi6790/utility/Constants.java
+++ src/main/java/de/timmi6790/utility/Constants.java
@@ -1,5 +1,8 @@
 package de.timmi6790.utility;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class Constants
 {
 	public static final String MOD_ID = "utilitymod";
