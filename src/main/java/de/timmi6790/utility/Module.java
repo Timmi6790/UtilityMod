4b69b5c22cb63d4ad66afb59457b6fffb100906b
Tim <13711365+Timmi6790@users.noreply.github.com>
2023-05-10 18:24:34 +0200

    Add basic module system

---
+++ src/main/java/de/timmi6790/utility/Module.java
@@ -0,0 +1,19 @@
+package de.timmi6790.utility;
+
+public interface Module
+{
+	default void enable()
+	{
+
+	}
+
+	default void registerEvents()
+	{
+		
+	}
+
+	default void disable()
+	{
+
+	}
+}
--- src/main/java/de/timmi6790/utility/UtilityMod.java
+++ src/main/java/de/timmi6790/utility/UtilityMod.java
@@ -1,21 +1,77 @@
 package de.timmi6790.utility;
 
-import net.minecraft.init.Blocks;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 
 @Mod(modid = Constants.MOD_ID)
+@Log4j2
 public class UtilityMod
 {
 	@Getter
-	@Mod.Instance(value = Constants.MOD_ID)
+	@Mod.Instance(Constants.MOD_ID)
 	private static UtilityMod instance;
 
-	@Mod.EventHandler
-	public void init(final FMLInitializationEvent event)
+	private final Map<Class<? extends Module>, Module> modules = new HashMap<>();
+
+	public UtilityMod()
 	{
-		System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
+
+	}
+
+	public <T extends Module> Optional<T> getModule(final Class<T> clazz)
+	{
+		final T module = (T) this.modules.get(clazz);
+		return Optional.ofNullable(module);
+	}
+
+	void addModules(final boolean registerEvents, final Module... modules)
+	{
+		for (final Module module : modules)
+		{
+			this.addModule(module, registerEvents);
+		}
+	}
+
+	void addModule(final Module module, final boolean registerEvents)
+	{
+		try
+		{
+			log.debug("Loading module: " + module.getClass().getName() + " with registerEvents: " + registerEvents);
+			module.enable();
+			if (registerEvents)
+			{
+				module.registerEvents();
+			}
+
+			this.modules.put(module.getClass(), module);
+		}
+		catch (final Exception e)
+		{
+			log.error("Failed to load module: " + module.getClass().getName(), e);
+		}
+	}
+
+	@Mod.EventHandler
+	public void preInit(final FMLPreInitializationEvent event)
+	{
+		for (final Module module : this.modules.values())
+		{
+			try
+			{
+				log.debug("Register events for module: " + module.getClass().getName());
+				module.registerEvents();
+			}
+			catch (final Exception e)
+			{
+				log.error("Failed to register events for module: " + module.getClass().getName(), e);
+			}
+		}
 	}
 }
