51a9454b3197021d3ebbcc98590b8d22289f929c
Tim <13711365+Timmi6790@users.noreply.github.com>
2023-05-10 14:23:03 +0200

    Add mod instance to main

--- src/main/java/de/timmi6790/utility/UtilityMod.java
+++ src/main/java/de/timmi6790/utility/UtilityMod.java
@@ -4,9 +4,15 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
+import lombok.Getter;
+
 @Mod(modid = Constants.MOD_ID)
 public class UtilityMod
 {
+	@Getter
+	@Mod.Instance(value = Constants.MOD_ID)
+	private static UtilityMod instance;
+
 	@Mod.EventHandler
 	public void init(final FMLInitializationEvent event)
 	{
