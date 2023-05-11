272fa1b9b9975580e3303f79c06953f2d229f42b
Tim <13711365+Timmi6790@users.noreply.github.com>
2023-05-11 15:59:10 +0200

    Add PlayerUtil

---
+++ src/main/java/de/timmi6790/utility/utils/PlayerUtil.java
@@ -0,0 +1,30 @@
+package de.timmi6790.utility.utils;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PlayerUtil
+{
+	public Optional<EntityPlayer> getPlayer()
+	{
+		return Optional.ofNullable(Minecraft.getMinecraft().thePlayer);
+	}
+
+	public boolean consumePlayer(Consumer<EntityPlayer> playerConsumer)
+	{
+		final Optional<EntityPlayer> playerOptional = PlayerUtil.getPlayer();
+		if (!playerOptional.isPresent())
+		{
+			return false;
+		}
+
+		playerConsumer.accept(playerOptional.get());
+		return true;
+	}
+}
